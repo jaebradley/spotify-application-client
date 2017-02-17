d451254e6b26452dc05688e5f7fc09dcba983723
jaebradley <jae.b.bradley@gmail.com>
2017-02-17 13:10:39 -0500

    initial spotify client implementation

---
+++ src/SpotifyClient.js
@@ -0,0 +1,107 @@
+'use babel';
+
+import osascript from 'node-osascript';
+
+import SpotifyCommands from '../data/SpotifyCommands';
+import PlayerState from '../data/PlayerState';
+
+export default class SpotifyClient {
+  static isSpotifyRunning() {
+    return SpotifyClient.executeCommand(SpotifyCommands.IS_SPOTIFY_RUNNING);
+  }
+
+  static getSongName() {
+    return SpotifyClient.executeCommand(SpotifyCommands.GET_SONG_NAME);
+  }
+
+  static getAlbumName() {
+    return SpotifyClient.executeCommand(SpotifyCommands.GET_ALBUM_NAME);
+  }
+
+  static getArtistName() {
+    return SpotifyClient.executeCommand(SpotifyCommands.GET_ARTIST_NAME);
+  }
+
+  static getPlayerState() {
+    return SpotifyClient.executeCommand(SpotifyCommands.GET_PLAYER_STATE)
+                        .then(stateValue => PlayerState.valueOf(stateValue));
+  }
+
+  static getPlayerPositionInSeconds() {
+    return SpotifyClient.executeCommand(SpotifyCommands.GET_PLAYER_POSITION_IN_SECONDS);
+  }
+
+  static getSongDurationInSeconds() {
+    return SpotifyClient.executeCommand(SpotifyCommands.GET_SONG_DURATION_IN_SECONDS);
+  }
+
+  static turnOffRepeat() {
+    return SpotifyClient.executeCommand(SpotifyCommands.TURN_OFF_REPEAT);
+  }
+
+  static turnOnRepeat() {
+    return SpotifyClient.executeCommand(SpotifyCommands.TURN_ON_REPEAT);
+  }
+
+  static isRepeating() {
+    return SpotifyClient.executeCommand(SpotifyCommands.IS_REPEATING);
+  }
+
+  static toggleRepeat() {
+    return SpotifyClient.isRepeating()
+                        .then(isRepeating => {
+                          if (isRepeating) {
+                            return SpotifyClient.turnOffRepeat();
+                          }
+
+                          return SpotifyClient.turnOnRepeat();
+                        });
+  }
+
+  static turnOffShuffle() {
+    return SpotifyClient.executeCommand(SpotifyCommands.TURN_OFF_SHUFFLE);
+  }
+
+  static turnOnShuffle() {
+    return SpotifyClient.executeCommand(SpotifyCommands.TURN_ON_SHUFFLE);
+  }
+
+  static isShuffling() {
+    return SpotifyClient.executeCommand(SpotifyCommands.IS_SHUFFLING);
+  }
+
+  static toggleShuffle() {
+    return SpotifyClient.isShuffling()
+                        .then(isShuffling => {
+                          if (isShuffling) {
+                            return SpotifyClient.turnOffShuffle();
+                          } else {
+                            return SpotifyClient.turnOnShuffle();
+                          }
+                        });
+  }
+
+  static togglePlayPause() {
+    return SpotifyClient.executeCommand(SpotifyCommands.TOGGLE_PLAY_PAUSE);
+  }
+
+  static goToNextTrack() {
+    return SpotifyClient.executeCommand(SpotifyCommands.GO_TO_NEXT_TRACK);
+  }
+
+  static goToPreviousTrack() {
+    return SpotifyClient.executeCommand(SpotifyCommands.GO_TO_PREVIOUS_TRACK);
+  }
+
+  static executeCommand(command) {
+    return new Promise((resolve, reject) => {
+      osascript.execute(command.value, function(err, result, raw) {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject({ reason: 'Unable to execute command' });
+        }
+      });
+    });
+  }
+}
--- src/data/SpotifyCommands.js
+++ src/data/SpotifyCommands.js
@@ -1,13 +1,13 @@
 'use es6';
 
 // From Spotify.sdef file
-export const GET_SONG  = 'tell application "Spotify" to name of current track';
-export const GET_ALBUM = 'tell application "Spotify" to album of current track';
+export const GET_TRACK_NAME  = 'tell application "Spotify" to name of current track';
+export const GET_ALBUM_NAME = 'tell application "Spotify" to album of current track';
 export const GET_ALBUM_ARTWORK_URL = 'tell application "Spotify" to artwork url of current track';
-export const GET_ARTIST = 'tell application "Spotify" to artist of current track';
+export const GET_ARTIST_NAME = 'tell application "Spotify" to artist of current track';
 export const GET_PLAYER_STATE = 'tell application "Spotify" to player state';
-export const GET_PLAYER_POSITION = 'tell application "Spotify" to player position';
-export const GET_SONG_DURATION = 'tell application "Spotify" to duration of current track';
+export const GET_PLAYER_POSITION_IN_SECONDS = 'tell application "Spotify" to player position';
+export const GET_SONG_DURATION_IN_SECONDS = 'tell application "Spotify" to duration of current track';
 export const TURN_OFF_REPEAT = 'tell application "Spotify" to set repeating to false';
 export const TURN_ON_REPEAT = 'tell application "Spotify" to set repeating to true';
 export const TURN_OFF_SHUFFLE = 'tell application "Spotify" to set shuffling to false';
