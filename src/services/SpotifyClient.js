22b75f0bf1f698d6beb2ad26496e8cd737d62db5
jaebradley <jae.b.bradley@gmail.com>
2017-02-17 13:32:48 -0500

    implement command execution service in client

--- src/services/SpotifyClient.js
+++ src/services/SpotifyClient.js
@@ -1,50 +1,50 @@
 'use es6';
 
-import osascript from 'node-osascript';
+import CommandExecutor from './CommandExecutor';
 
-import SpotifyCommands from '../data/SpotifyCommands';
 import PlayerState from '../data/PlayerState';
+import SpotifyCommands from '../data/SpotifyCommands';
 
 export default class SpotifyClient {
   static isSpotifyRunning() {
-    return SpotifyClient.executeCommand(SpotifyCommands.IS_SPOTIFY_RUNNING);
+    return CommandExecutor.execute(SpotifyCommands.IS_SPOTIFY_RUNNING);
   }
 
   static getSongName() {
-    return SpotifyClient.executeCommand(SpotifyCommands.GET_SONG_NAME);
+    return CommandExecutor.execute(SpotifyCommands.GET_SONG_NAME);
   }
 
   static getAlbumName() {
-    return SpotifyClient.executeCommand(SpotifyCommands.GET_ALBUM_NAME);
+    return CommandExecutor.execute(SpotifyCommands.GET_ALBUM_NAME);
   }
 
   static getArtistName() {
-    return SpotifyClient.executeCommand(SpotifyCommands.GET_ARTIST_NAME);
+    return CommandExecutor.execute(SpotifyCommands.GET_ARTIST_NAME);
   }
 
   static getPlayerState() {
-    return SpotifyClient.executeCommand(SpotifyCommands.GET_PLAYER_STATE)
-                        .then(stateValue => PlayerState.valueOf(stateValue));
+    return CommandExecutor.execute(SpotifyCommands.GET_PLAYER_STATE)
+                          .then(stateValue => PlayerState.valueOf(stateValue));
   }
 
   static getPlayerPositionInSeconds() {
-    return SpotifyClient.executeCommand(SpotifyCommands.GET_PLAYER_POSITION_IN_SECONDS);
+    return CommandExecutor.execute(SpotifyCommands.GET_PLAYER_POSITION_IN_SECONDS);
   }
 
   static getTrackDurationInSeconds() {
-    return SpotifyClient.executeCommand(SpotifyCommands.GET_TRACK_DURATION_IN_SECONDS);
+    return CommandExecutor.execute(SpotifyCommands.GET_TRACK_DURATION_IN_SECONDS);
   }
 
   static turnOffRepeat() {
-    return SpotifyClient.executeCommand(SpotifyCommands.TURN_OFF_REPEAT);
+    return CommandExecutor.execute(SpotifyCommands.TURN_OFF_REPEAT);
   }
 
   static turnOnRepeat() {
-    return SpotifyClient.executeCommand(SpotifyCommands.TURN_ON_REPEAT);
+    return CommandExecutor.execute(SpotifyCommands.TURN_ON_REPEAT);
   }
 
   static isRepeating() {
-    return SpotifyClient.executeCommand(SpotifyCommands.IS_REPEATING);
+    return CommandExecutor.execute(SpotifyCommands.IS_REPEATING);
   }
 
   static toggleRepeat() {
@@ -59,15 +59,15 @@ export default class SpotifyClient {
   }
 
   static turnOffShuffle() {
-    return SpotifyClient.executeCommand(SpotifyCommands.TURN_OFF_SHUFFLE);
+    return CommandExecutor.execute(SpotifyCommands.TURN_OFF_SHUFFLE);
   }
 
   static turnOnShuffle() {
-    return SpotifyClient.executeCommand(SpotifyCommands.TURN_ON_SHUFFLE);
+    return CommandExecutor.execute(SpotifyCommands.TURN_ON_SHUFFLE);
   }
 
   static isShuffling() {
-    return SpotifyClient.executeCommand(SpotifyCommands.IS_SHUFFLING);
+    return CommandExecutor.execute(SpotifyCommands.IS_SHUFFLING);
   }
 
   static toggleShuffle() {
@@ -81,34 +81,22 @@ export default class SpotifyClient {
   }
 
   static togglePlayPause() {
-    return SpotifyClient.executeCommand(SpotifyCommands.TOGGLE_PLAY_PAUSE);
+    return CommandExecutor.execute(SpotifyCommands.TOGGLE_PLAY_PAUSE);
   }
 
   static play() {
-    return SpotifyClient.executeCommand(SpotifyCommands.PLAY);
+    return CommandExecutor.execute(SpotifyCommands.PLAY);
   }
 
   static pause() {
-    return SpotifyClient.executeCommand(SpotifyCommands.PAUSE);
+    return CommandExecutor.execute(SpotifyCommands.PAUSE);
   }
 
   static goToNextTrack() {
-    return SpotifyClient.executeCommand(SpotifyCommands.GO_TO_NEXT_TRACK);
+    return CommandExecutor.execute(SpotifyCommands.GO_TO_NEXT_TRACK);
   }
 
   static goToPreviousTrack() {
-    return SpotifyClient.executeCommand(SpotifyCommands.GO_TO_PREVIOUS_TRACK);
-  }
-
-  static executeCommand(command) {
-    return new Promise((resolve, reject) => {
-      osascript.execute(command.value, function(err, result, raw) {
-        if (!err) {
-          resolve(result);
-        } else {
-          reject({ reason: `Unable to execute command: ${command}` });
-        }
-      });
-    });
+    return CommandExecutor.execute(SpotifyCommands.GO_TO_PREVIOUS_TRACK);
   }
 }
