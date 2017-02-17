85dc321a138dd8a7cc5eddbbcc75369b75a7b9ec
jaebradley <jae.b.bradley@gmail.com>
2017-02-17 13:18:06 -0500

    add index

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+'use es6';
+
+import SpotifyClient from './SpotifyClient';
+import PlayerState from '../data/PlayerState';
+
+module.exports = {
+  SpotifyClient: SpotifyClient,
+  PlayerState: PlayerState
+};
