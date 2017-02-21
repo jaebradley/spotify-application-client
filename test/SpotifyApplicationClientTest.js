94b7b2b662eb60d2d6733dde883bafe658bc5227
jaebradley <jae.b.bradley@gmail.com>
2017-02-20 23:03:39 -0500

    remove unused imports

--- test/SpotifyApplicationClientTest.js
+++ test/SpotifyApplicationClientTest.js
@@ -3,9 +3,6 @@
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 chai.use(chaiAsPromised);
-
-const expect = chai.expect;
-const assert = chai.assert;
 chai.should();
 
 import SpotifyApplicationClient from '../src/services/SpotifyApplicationClient';
