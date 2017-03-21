7ce77515283bd83ff89d11ba67843cf5c745c05c
jaebradley <jae.b.bradley@gmail.com>
2017-03-20 21:03:03 -0400

    fix field name

--- test/SpotifyApplicationClientIntegrationTest.js
+++ test/SpotifyApplicationClientIntegrationTest.js
@@ -26,7 +26,7 @@ const checkTrackState = function(trackName, albumName, artistName, trackDuration
     name: trackName,
     albumName: albumName,
     artistName: artistName,
-    trackDurationInMilliseconds: trackDuration
+    durationInMilliseconds: trackDuration
   });
   return SpotifyApplicationClient.getTrackDetails()
     .then(details => {
